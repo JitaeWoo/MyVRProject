c94d69f0768f3f2cfba2b5e905e5e195af7b09fe
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-13 16:46:57 +0900

    [Feat] ShootingTarget 스트립트 추가
    총알에 맞으면 뒤로 넘어갔다가 돌아오는 연출을 담당할 ShootingTarget 스크립트 추가.

---
+++ Assets/Scripts/ShootingTarget.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ShootingTarget : MonoBehaviour
+{
+    [SerializeField] private Transform _joint;
+
+    private Vector3 _attackedRotate = new Vector3(90, 0, 0);
+
+    private WaitForSeconds _delay = new WaitForSeconds(3f);
+    private Coroutine _delayCoroutine;
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Bullet"))
+        {
+            if(_delayCoroutine == null)
+            {
+                _delayCoroutine = StartCoroutine(AttackedTarget());
+            }
+        }
+    }
+
+    private IEnumerator AttackedTarget()
+    {
+        _joint.DORotate(_attackedRotate, 1f);
+
+        yield return _delay;
+
+        _joint.DORotate(Vector3.zero, 1f);
+    }
+}

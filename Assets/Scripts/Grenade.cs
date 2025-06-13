97ead0e3278a0c9d8773d58ec0b89f0371087aea
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-13 14:47:11 +0900

    [Feat] Grenade 스크립트 추가
    핀을  뽑았을 때 10초 뒤에 폭발하도록 간단한 로직 작성

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Grenade : MonoBehaviour
+{
+    [SerializeField] private GameObject _pinPrefab;
+    [SerializeField] private Transform _pinTransform;
+
+    private WaitForSeconds _delay;
+    private float _countTime = 5f;
+
+    private void Awake() => Init();
+
+    private void Init()
+    {
+        _delay = new WaitForSeconds(_countTime);
+        Instantiate(_pinPrefab, _pinTransform.position, _pinTransform.rotation);
+    }
+
+    public void PinOut(SelectExitEventArgs args)
+    {
+        args.interactableObject.transform.gameObject.layer = LayerMask.NameToLayer("Default");
+        StartCoroutine(StartCountDown());
+    }
+
+    private void Explosion()
+    {
+        Debug.Log("Explosion");
+    }
+
+    private IEnumerator StartCountDown()
+    {
+        yield return _delay;
+
+        Explosion();
+    }
+}
